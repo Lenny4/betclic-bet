0700c2e14e605e4964af98a3249aa4f339868d3e
Alexandre Beaujour <alexandre.beaujour@hotmail.fr>
2020-10-10 14:51:22 +0200

    try catch for error

--- src/app.js
+++ src/app.js
@@ -49,49 +49,55 @@ class App {
         const bet = this.bets[0];
         console.log('is betting on ', bet);
         let page = await this.browser.newPage();
-        console.log('page created for bet');
-        const url = 'https://www.betclic.fr/' + '-m' + bet.matchId;
-        console.log('page going to ' + url);
-        await page.goto(url);
-        await page.addScriptTag({path: 'lib/jquery-3.4.1.min.js'});
-        console.log('page before login');
-        page = await this.login(page);
-        console.log('start canBet');
-        let r = true;
-        if (page.url().includes(bet.matchId)) {
-            await this.clearPanier(page);
-            let buttonSelector = 'app-match > div > app-match-markets > app-market:nth-child(1) > div > div.ng-star-inserted > div > div > ';
-            if (bet.choiceName.toLowerCase() === '%1%') {
-                buttonSelector = 'div:nth-child(1) > app-selection';
-            } else if (bet.choiceName.toLowerCase() === '%2%') {
-                buttonSelector = 'div:nth-child(3) > app-selection';
-            } else if (bet.choiceName.toLowerCase() === 'nul') {
-                buttonSelector = 'div:nth-child(2) > app-selection';
-            } else {
-                r = false;
-                console.log('Error : no bet.choiceName defined for ' + bet.choiceName);
-            }
-            if (r) {
-                console.log('click on odd ...');
-                await page.click(buttonSelector);
-                await this.timeout(500);
-                console.log('enter amount ...');
-                await page.type('app-betting-slip-single-bet-item-footer > div > div > app-bs-stake > div > input', process.env.AMOUNT_BET);
-                await this.timeout(500);
-                if (await page.$('bc-gb-cookie-banner > div > div > button') !== null) {
-                    console.log('click remove cookie ...');
-                    await page.click('bc-gb-cookie-banner > div > div > button');
-                    await this.timeout(500);
+        try {
+            console.log('start try, page created for bet');
+            const url = 'https://www.betclic.fr/' + '-m' + bet.matchId;
+            console.log('page going to ' + url);
+            await page.goto(url);
+            await page.addScriptTag({path: 'lib/jquery-3.4.1.min.js'});
+            console.log('page before login');
+            page = await this.login(page);
+            console.log('start canBet');
+            let r = true;
+            if (page.url().includes(bet.matchId)) {
+                await this.clearPanier(page);
+                let buttonSelector = 'app-match > div > app-match-markets > app-market:nth-child(1) > div > div.ng-star-inserted > div > div > ';
+                if (bet.choiceName.toLowerCase() === '%1%') {
+                    buttonSelector = 'div:nth-child(1) > app-selection';
+                } else if (bet.choiceName.toLowerCase() === '%2%') {
+                    buttonSelector = 'div:nth-child(3) > app-selection';
+                } else if (bet.choiceName.toLowerCase() === 'nul') {
+                    buttonSelector = 'div:nth-child(2) > app-selection';
+                } else {
+                    r = false;
+                    console.log('Error : no bet.choiceName defined for ' + bet.choiceName);
                 }
-                console.log('click Parier ...');
-                await page.click('#betBtn');
+                if (r) {
+                    console.log('click on odd ...');
+                    await page.click(buttonSelector);
+                    await this.timeout(500);
+                    console.log('enter amount ...');
+                    await page.type('app-betting-slip-single-bet-item-footer > div > div > app-bs-stake > div > input', process.env.AMOUNT_BET);
+                    await this.timeout(500);
+                    if (await page.$('bc-gb-cookie-banner > div > div > button') !== null) {
+                        console.log('click remove cookie ...');
+                        await page.click('bc-gb-cookie-banner > div > div > button');
+                        await this.timeout(500);
+                    }
+                    console.log('click Parier ...');
+                    await page.click('#betBtn');
+                }
+            } else {
+                console.log(bet.matchName + ' is not available on betclic : ' + page.url());
             }
-        } else {
-            console.log(bet.matchName + ' is not available on betclic : ' + page.url());
+            this.bets.splice(0, 1);
+            await this.clearPanier(page);
+            await this.timeout(2000);
+        } catch (e) {
+            console.log('==============================================================================');
+            console.log(e);
+            console.log('==============================================================================');
         }
-        this.bets.splice(0, 1);
-        await this.clearPanier(page);
-        await this.timeout(2000);
         await page.goto('about:blank');
         await page.close();
         this.bet();
@@ -124,9 +130,13 @@ class App {
         await loginFormVisible;
         const loginDone = page.waitForSelector('body > app-desktop > bc-gb-header > header > div > a.header_account.prebootFreeze.ng-star-inserted > span', {visible: true});
         await this.timeout(500);
-        await page.type('#loginPage_username > input', process.env.LOGIN_USERNAME);
+        const loginSelector = '#loginPage_username > input';
+        await this.deleteInputValue(page, loginSelector);
+        await page.type(loginSelector, process.env.LOGIN_USERNAME);
         await this.timeout(500);
-        await page.type('#loginpage_password > input', process.env.LOGIN_PASSWORD);
+        const passwordSelector = '#loginpage_password > input';
+        await this.deleteInputValue(page, passwordSelector);
+        await page.type(passwordSelector, process.env.LOGIN_PASSWORD);
         await this.timeout(500);
         await page.type('#date', process.env.LOGIN_DAY + process.env.LOGIN_MONTH + process.env.LOGIN_YEAR);
         await this.timeout(500);
@@ -135,6 +145,14 @@ class App {
         await loginDone;
         console.log('logging done !');
         return page;
+    }
+
+    async deleteInputValue(page, selector) {
+        await page.focus(selector);
+        await page.keyboard.down('Control');
+        await page.keyboard.press('A');
+        await page.keyboard.up('Control');
+        await page.keyboard.press('Backspace');
     }
 
     /**
